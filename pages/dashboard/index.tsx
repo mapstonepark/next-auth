3fd10bee0df7c33604f147e2e2484aeceba8e250
Jamal Park <102711674+mapstonepark@users.noreply.github.com>
2023-05-20 15:11:49 -0500

    working with prisma for mongodb

--- pages/dashboard/index.tsx
+++ pages/dashboard/index.tsx
@@ -2,6 +2,9 @@ import Header from "../../components/header";
 
 export default function Index() {
     return (
+        <>
       <Header />
+      <h1>Welcome! Please sign in!</h1>
+      </>
     )
   }
